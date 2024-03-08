24fdcb3faab67aca9712b5bae08b3e41c6723fa2
Ravi Billa <billaravi@gmail.com>
2024-03-07 16:50:22 -0800

    snipper 012

---
+++ 012_print_emojis.py
@@ -0,0 +1,8 @@
+# Print emojis
+#!pip install emoji
+import emoji
+
+print(emoji.emojize("I climbed Mt.Fuji during my receent trip to Japan :mount_fuji:"))
+print(emoji.emojize("Let's play Rock, Paper, Scissors :rock::rolled-up_newspaper::scissors:"))
+
+# https://carpedm20.github.io/emoji/
