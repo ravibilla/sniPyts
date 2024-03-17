71d00835287d01be72a628cf300f6e719d5cf061
Ravi Billa <billaravi@gmail.com>
2024-03-17 16:11:24 -0700

    snippet font art

---
+++ create_font_art.py
@@ -0,0 +1,9 @@
+# Create font art
+#!pip install pyfiglet
+import pyfiglet
+
+font = pyfiglet.figlet_format('Ravi Billa')
+font = pyfiglet.figlet_format('Ravi Billa', font='bulbhead')
+print(font)
+
+# List of all fonts here: --> http://www.figlet.org/fontdb.cgi
